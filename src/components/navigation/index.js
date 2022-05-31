f8d1f972e8cacba75fa1a7aba5d81ab4cb393b26
LunguAndreiLeonard <andruleo99@gmail.com>
2022-05-31 17:24:20 +0300

    master

--- src/components/navigation/index.js
+++ src/components/navigation/index.js
@@ -1,9 +1,9 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import * as React from 'react'
 import { NavigationContainer} from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 
-import SignInScreen from '../screens/SignInScreen'; 
+import SignInScreen from '../../screens/SignInScreen';
+ 
 
 
 
@@ -16,15 +16,13 @@ import ConfirmPasswordScreen from '../screens/ConfirmPasswordScreen';
 
 const Stack = createNativeStackNavigator(); 
 
-const Navigation = () => {
+function Navigation() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator> 
-          <Stack.Screen name="SignIn" component={SignInScreen} />
-
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Stack.Navigator
+    screenOptions={{headerShown: false}}>
+      
+      <Stack.Screen name="SignIn" component={SignInScreen} />
+    </Stack.Navigator>
   );
-};
-
+  }
 export default Navigation;
